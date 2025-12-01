951f5e6d4c4478892a8e8cf8c93218fa866fa8be
Ahmed Ayman <ahamed.ayman220@gmail.com>
2025-12-01 06:51:37 +0200

    Fix route controller namespace duplication issue
    - Removed full namespace path from route definition
    - Use imported class name instead

--- platform/plugins/multi-country-sync/routes/web.php
+++ platform/plugins/multi-country-sync/routes/web.php
@@ -24,7 +24,7 @@ AdminHelper::registerRoutes(function (): void {
 
                 Route::post('test-connection', [
                     'as' => 'settings.test-connection',
-                    'uses' => \Botble\MultiCountrySync\Http\Controllers\Api\TestConnectionController::class . '@test',
+                    'uses' => TestConnectionController::class . '@test',
                     'permission' => 'multi-country-sync.settings',
                 ]);
 
